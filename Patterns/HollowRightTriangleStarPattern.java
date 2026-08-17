090f3cd837fb908c9d9c0565248cf215abf4d0dc
sukesh-006 <sukeshvsb@gmail.com>
2026-08-17 14:53:26 +0530

    Add HollowRightTriangleStarPattern.java - star pattern program

---
+++ Patterns/HollowRightTriangleStarPattern.java
@@ -0,0 +1,22 @@
+// Hollow Right Triangle Star Pattern
+// *
+// * *
+// *   *
+// *     *
+// * * * * *
+
+public class HollowRightTriangleStarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
