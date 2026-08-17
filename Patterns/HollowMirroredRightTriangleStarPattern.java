9ab718b5c5e0ba78718155d9caf8eb9977b9d2c1
sukesh-006 <sukeshvsb@gmail.com>
2026-08-17 14:53:44 +0530

    Add HollowMirroredRightTriangleStarPattern.java - star pattern program

---
+++ Patterns/HollowMirroredRightTriangleStarPattern.java
@@ -0,0 +1,25 @@
+// Hollow Mirrored Right Triangle Star Pattern
+//         *
+//       * *
+//     *   *
+//   *     *
+// * * * * *
+
+public class HollowMirroredRightTriangleStarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
