9971191158cfa367bd11f019f9fb40adc74fbfe6
sukesh-006 <sukeshvsb@gmail.com>
2026-08-17 14:55:11 +0530

    Add HalfDiamondStarPattern.java - star pattern program

---
+++ Patterns/HalfDiamondStarPattern.java
@@ -0,0 +1,30 @@
+// Half Diamond Star Pattern
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+public class HalfDiamondStarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
