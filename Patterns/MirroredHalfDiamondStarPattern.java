201f1b8a9548b5228e39bd84d965e6eb2ed87903
sukesh-006 <sukeshvsb@gmail.com>
2026-08-17 14:55:26 +0530

    Add MirroredHalfDiamondStarPattern.java - star pattern program

---
+++ Patterns/MirroredHalfDiamondStarPattern.java
@@ -0,0 +1,36 @@
+// Mirrored Half Diamond Star Pattern
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+//   * * * *
+//     * * *
+//       * *
+//         *
+
+public class MirroredHalfDiamondStarPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
