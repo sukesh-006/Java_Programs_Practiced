87fe7da9a52f75ffca7dc49443ee036ef8fe2c38
sukesh-006 <sukeshvsb@gmail.com>
2026-08-28 10:13:47 +0530

    Add DSA/BinarySearch.java

---
+++ DSA/BinarySearch.java
@@ -0,0 +1,15 @@
+public class BinarySearch {
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        System.out.println("Index: " + search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+    }
+}
