c5e3e1d68b17f7e5db720dc3d615376d40e37188
sukesh-006 <sukeshvsb@gmail.com>
2026-08-28 10:24:54 +0530

    Add DSA/QueueUsingStacks.java

---
+++ DSA/QueueUsingStacks.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+public class QueueUsingStacks {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+    public void enqueue(int x) {
+        s1.push(x);
+    }
+    public int dequeue() {
+        if (s2.isEmpty()) {
+            if (s1.isEmpty()) return -1;
+            while (!s1.isEmpty()) s2.push(s1.pop());
+        }
+        return s2.pop();
+    }
+    public static void main(String[] args) {
+        QueueUsingStacks q = new QueueUsingStacks();
+        q.enqueue(1);
+        q.enqueue(2);
+        System.out.println("Dequeued: " + q.dequeue());
+    }
+}
